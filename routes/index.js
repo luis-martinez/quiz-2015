cecb558204f58477c1dc5b22af450e09ed4fad63
Luis Martínez <-->
2015-07-09 19:25:28 +0200

    author

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,9 @@ router.get('/', function(req, res, next) {
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
+/* GET author */
+router.get('/author', function(req, res) {
+  res.render('author');
+});
+
 module.exports = router;
