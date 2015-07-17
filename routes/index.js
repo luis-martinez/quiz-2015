362d0d86bee1a96b31570c2d6776b6ba0b8d1d68
Luis Martínez <-->
2015-07-17 14:08:00 +0200

    autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,6 +14,11 @@ router.get('/', function(req, res) {
 router.get('/author', function(req, res) {
   res.render('author', {errors: []});
 });
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
@@ -27,6 +33,7 @@ router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
 router.put('/quizes/:quizId(\\d+)',        quizController.update);
 router.delete('/quizes/:quizId(\\d+)',     quizController.destroy);
 
+// Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 
